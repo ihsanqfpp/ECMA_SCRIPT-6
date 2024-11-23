1b2b41586d8b5ba3ae576311af03ebc1eeb42a5c
ihsanqfpp <okzihsan956@gmail.com>
2024-11-23 22:40:51 +0500

    init

--- variable_scoping.js
+++ variable_scoping.js
@@ -18,10 +18,33 @@
 // //console.log(b);//this show the output is undefined
 // console.log(c)//show the output is 30
 
-let a = 400;
-{
-    let a = 10;
-    const b = 20;
-    console.log("INNER : " ,a)
-}
-console.log(a)
+// let a = 400;
+// {
+//     let a = 10;
+//     const b = 20;
+//     console.log("INNER : " ,a)
+// }
+// console.log(a)
+
+//we access the parent child in child
+// {
+//     let a = 10;
+//     const b = 20;
+//     {
+//         console.log(a)//we access the parent child in child
+//     }
+//     console.log(b)
+// }
+
+// //but  can not  access the child in parent 
+// {
+//     let a = 10;
+//     const b = 20;
+//     {
+//        let c = 20;
+//         console.log(a)
+
+//     }
+//     console.log(c)//but  can not  access the child in parent  
+// }
+
